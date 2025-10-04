4db3598864c0f014990953e6c3d37b29d842eda1
Jannatul Ferdous <137918726+Jannatul-100@users.noreply.github.com>
2025-10-04 08:27:38 +0600

    Commit last

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -3,7 +3,7 @@ import Bookmark from '../Bookmark/Bookmark';
 
 const Bookmarks = ({bookmarks, readingTime}) => {
     return (
-        <div className='md:w-1/3 my-4 bg-gray-200 ml-6 rounded-lg'>
+        <div className='md:w-1/3 my-6 bg-gray-200 ml-6 rounded-lg'>
             <div className='mt-6 bg-[#6047EC]/10 border-2 border-purple-700  p-4 m-6 rounded-lg'>
                 <h3 className='text-2xl text-center text-purple-700 font-bold'>Spent Time on Read: {readingTime}</h3>
             </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <header className='flex justify-between items-center p-3 border-b border-gray-300 max-w-7xl mx-auto'>
+        <header className='flex justify-between items-center p-4 border-b border-gray-300 max-w-7xl mx-auto'>
             <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
-            <img src={profile}></img>
+            <img className='w-12 h-12' src={profile}></img>
         </header>
     );
 };
